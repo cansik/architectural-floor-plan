5dbfee9ff761fa95f988f4157bdfb67b9a160825
Alexander Wyss <alexander.wyss@students.fhnw.ch>
2016-09-29 14:55:34 +0200

    Added OpenCv native libraries

--- Afpars/src/ch/fhnw/afpars/Main.kt
+++ Afpars/src/ch/fhnw/afpars/Main.kt
@@ -3,12 +3,18 @@
  */
 package ch.fhnw.afpars
 
+import org.opencv.core.Core
+import org.opencv.core.CvType
+import org.opencv.core.Mat
+
 class Main {
 
     companion object {
         @JvmStatic
         fun main(args: Array<String>) {
-            System.out.println("Hello from Afpars")
+            System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+            val mat = Mat.eye(3, 3, CvType.CV_8UC1);
+            System.out.println("mat = " + mat.dump());
         }
     }
 }
