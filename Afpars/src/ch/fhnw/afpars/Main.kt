01d4684161fc9414a75652b17ca4ed48f9e01fdc
Florian Broox <florian@nexpose.ch>
2016-09-29 14:42:14 +0200

    created basic project

---
+++ Afpars/src/ch/fhnw/afpars/Main.kt
@@ -0,0 +1,14 @@
+/**
+ * Created by cansik on 29.09.16.
+ */
+package ch.fhnw.afpars
+
+class Main {
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            System.out.println("Hello from Afpars")
+        }
+    }
+}
