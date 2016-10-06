654b5deb09672fa066db520adea1794692aa99b5
Florian Bruggisser <florian@nexpose.ch>
2016-10-06 23:34:00 +0200

    added gradle build support to project

--- afpars/src/main/kotlin/ch/fhnw/afpars/Main.kt
+++ afpars/src/main/kotlin/ch/fhnw/afpars/Main.kt
@@ -11,7 +11,7 @@ class Main : Application() {
     @Throws(Exception::class)
     override fun start(primaryStage: Stage) {
         val root = FXMLLoader.load<Parent>(javaClass.classLoader.getResource("view/MainView.fxml"))
-        primaryStage.title = "Afpars"
+        primaryStage.title = "Architectural Floor Plan Analysis"
         primaryStage.scene = Scene(root)
         primaryStage.show()
         System.out.println()
