9c10454f86e1a12f26e01a8df4fe1e19b4ab6e26
Florian Bruggisser <florian@nexpose.ch>
2016-10-07 17:20:06 +0200

    added missing IAlgorithm flag

--- afpars/src/main/kotlin/ch/fhnw/afpars/algorithm/objectdetection/IObjectDetectionAlgorithm.kt
+++ afpars/src/main/kotlin/ch/fhnw/afpars/algorithm/objectdetection/IObjectDetectionAlgorithm.kt
@@ -1,7 +1,9 @@
 package ch.fhnw.afpars.algorithm.objectdetection
 
+import ch.fhnw.afpars.algorithm.IAlgorithm
+
 /**
  * Created by cansik on 07.10.16.
  */
-interface IObjectDetectionAlgorithm {
+interface IObjectDetectionAlgorithm : IAlgorithm {
 }
--- afpars/src/main/kotlin/ch/fhnw/afpars/algorithm/zonemarking/IZoneMarkingAlgorithm.kt
+++ afpars/src/main/kotlin/ch/fhnw/afpars/algorithm/zonemarking/IZoneMarkingAlgorithm.kt
@@ -1,7 +1,9 @@
 package ch.fhnw.afpars.algorithm.zonemarking
 
+import ch.fhnw.afpars.algorithm.IAlgorithm
+
 /**
  * Created by cansik on 07.10.16.
  */
-interface IZoneMarkingAlgorithm {
+interface IZoneMarkingAlgorithm : IAlgorithm {
 }
