9e1f2afd1f3f478bebe6cece4260e697c2a5b9a4
Alexander Wyss <a.swyss@bluewin.ch>
2016-10-12 15:20:28 +0200

    Dilate View created

--- afpars/src/main/kotlin/ch/fhnw/afpars/algorithm/preprocessing/Dilate.kt
+++ afpars/src/main/kotlin/ch/fhnw/afpars/algorithm/preprocessing/Dilate.kt
@@ -1,18 +1,40 @@
 package ch.fhnw.afpars.algorithm.preprocessing
 
 import ch.fhnw.afpars.model.AFImage
+import ch.fhnw.afpars.ui.controller.DilateView
+import ch.fhnw.afpars.util.toImage
+import javafx.fxml.FXML
+import javafx.fxml.FXMLLoader
+import javafx.scene.Parent
+import javafx.scene.Scene
+import javafx.scene.control.Slider
+import javafx.scene.image.ImageView
+import javafx.stage.Modality
+import javafx.stage.Stage
+import javafx.stage.StageStyle
 import org.opencv.core.Mat
 import org.opencv.core.Size
 import org.opencv.imgproc.Imgproc
 
 class Dilate:IPreprocessingAlgorithm{
-    val dilationSize:Int
+    var dilationSize:Int
 
     constructor(dilationSize: Int){
         this.dilationSize = dilationSize
     }
 
     override fun run(imageAF: AFImage): AFImage {
+        System.out.println(dilationSize.toDouble())
+
+        val fxmlLoader = FXMLLoader(javaClass.classLoader.getResource("view/LocalView.fxml"))
+        val root1:Parent = fxmlLoader.load()
+        val controller = fxmlLoader.getController<DilateView>()
+        controller.setImage(imageAF)
+        controller.setDilationSize(dilationSize)
+        val stage = Stage()
+        stage.setTitle("Test")
+        stage.setScene(Scene(root1))
+        stage.showAndWait()
         return run(imageAF,dilationSize)
     }
 
