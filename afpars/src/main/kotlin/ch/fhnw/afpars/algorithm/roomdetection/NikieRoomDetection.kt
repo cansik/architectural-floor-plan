73dc7446578469b3d25d8c3be104b2a119066bd1
Alexander Wyss <alexander.wyss@students.fhnw.ch>
2016-11-03 15:57:46 +0100

    NikieRoomDetection update

--- afpars/src/main/kotlin/ch/fhnw/afpars/algorithm/roomdetection/NikieRoomDetection.kt
+++ afpars/src/main/kotlin/ch/fhnw/afpars/algorithm/roomdetection/NikieRoomDetection.kt
@@ -2,9 +2,9 @@ package ch.fhnw.afpars.algorithm.roomdetection
 
 import ch.fhnw.afpars.algorithm.AlgorithmParameter
 import ch.fhnw.afpars.model.AFImage
-import ch.fhnw.afpars.util.copy
-import ch.fhnw.afpars.util.zeros
-import org.opencv.core.*
+import ch.fhnw.afpars.util.*
+import org.opencv.core.Core
+import org.opencv.core.Scalar
 import org.opencv.imgproc.Imgproc
 
 /**
@@ -23,20 +23,97 @@ class NikieRoomDetection : IRoomDetectionAlgorithm {
     @AlgorithmParameter(name = "Threshold", minValue = 0.0, maxValue = 255.0)
     var treshold = 26.0
 
+
+    /*
+    Input ist ein morphologisch transformiertes Bild
+    Eingabetyp ist 32SC1
+     */
     override fun run(image: AFImage, history: MutableList<AFImage>): AFImage {
-        val img = image.clone()
-        var distTransform = img.image.zeros()
-        val distMinus = img.image.zeros()
-        val geodesicDistTransform = img.image.zeros()
-        var markers = img.image.zeros()
-        var markersBefore = img.image.zeros()
-        val invDistTransform = img.image.zeros()
-        val bindist = img.image.zeros()
+        //Originalbild
+        val original = image.clone()
+        //Distanztransformatin
+        val distTransform = original.image.zeros()
+        //GeodesicTransform
+        val geodesicTransform = original.image.zeros()
+        //Markers
+        val markers = original.image.zeros()
+        //Foreground
+        val foreground = original.image.zeros()
+
+
+        /*
+        Distanztranformation
+        Geht vom Originalbild aus, rückgabe ist das Bild "distTransform"
+        Konvertiert das Bild zu 8UC1
+         */
+        var localoriginal = original.image.zeros()
+        Imgproc.cvtColor(original.image, localoriginal, Imgproc.COLOR_BGR2GRAY)
+        Imgproc.distanceTransform(localoriginal, distTransform, Imgproc.CV_DIST_L2, Imgproc.CV_DIST_MASK_PRECISE)
+
+        //Background
+        val background = localoriginal.copy()
+        //Summed grounds
+        val summedUp = localoriginal.zeros()
+
+
+        /*
+        GeodesicDilation
+         */
+        val darkerDistTransform = original.image.zeros()
+        Core.subtract(distTransform, Scalar(differenceScalar), darkerDistTransform)
+        darkerDistTransform.geodesicDilate(distTransform, geodesicDilateSize, geodesicTransform)
+        Core.compare(distTransform, geodesicTransform, markers, Core.CMP_LE)
+
+
+        /*
+        Invert markers
+         */
+        Imgproc.threshold(markers, foreground, 128.0, 255.0, Imgproc.THRESH_BINARY_INV)
+
+
+        /*
+        Background
+         */
+        for (i in 0..background.height() - 1) {
+            for (j in 0..background.width() - 1) {
+                if (background.get(i, j)[0].equals(0.0)) {
+                    background.put(i, j, 128.0)
+                } else if (background.get(i, j)[0].equals(255.0)) {
+                    background.put(i, j, 0.0)
+                }
+            }
+        }
+
+        /*
+        Kombination Background/Foreground
+         */
+        for (i in 0..summedUp.height() - 1) {
+            for (j in 0..summedUp.width() - 1) {
+                summedUp.put(i, j, foreground.get(i, j)[0] + background.get(i, j)[0])
+            }
+        }
+
+        var watershedoriginal = localoriginal.copy()
+        Imgproc.cvtColor(localoriginal, watershedoriginal, Imgproc.COLOR_GRAY2BGR)
+        watershedoriginal = watershedoriginal.to8UC3()
+        val watershed = summedUp.to32S()
+
+        Imgproc.watershed(watershedoriginal, watershed)
+
+/*
+        //val img = image.clone()
+        //var distTransform = img.image.zeros()
+        val distMinus = original.image.zeros()
+        val geodesicDistTransform = original.image.zeros()
+        var markers = original.image.zeros()
+        var markersBefore = original.image.zeros()
+        val invDistTransform = original.image.zeros()
+        val bindist = original.image.zeros()
 
         // distance transform
-        Imgproc.cvtColor(img.image, img.image, Imgproc.COLOR_BGR2GRAY)
-        img.image.convertTo(img.image, CvType.CV_8UC1)
-        Imgproc.distanceTransform(img.image, distTransform, Imgproc.CV_DIST_L2, Imgproc.CV_DIST_MASK_PRECISE)
+        Imgproc.cvtColor(original.image, original.image, Imgproc.COLOR_BGR2GRAY)
+        original.image.convertTo(original.image, CvType.CV_8UC1)
+        Imgproc.distanceTransform(original.image, distTransform, Imgproc.CV_DIST_L2, Imgproc.CV_DIST_MASK_PRECISE)
 
 
         //normalize
@@ -56,7 +133,7 @@ class NikieRoomDetection : IRoomDetectionAlgorithm {
         history.add(AFImage(dist_8u.copy(), "Distu<8"))
         //Find total markers
         val contours = mutableListOf<MatOfPoint>()
-        val hierarchy = img.image.zeros()
+        val hierarchy = original.image.zeros()
         Imgproc.findContours(dist_8u, contours, hierarchy, Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE)
 
         // Create the marker image for the watershed algorithm
@@ -75,12 +152,29 @@ class NikieRoomDetection : IRoomDetectionAlgorithm {
 
         //Draw background markers
         Imgproc.circle(markers1, Point(5.0, 5.0), 3, Scalar(255.0, 255.0, 255.0), -1)
-
+        val uc8markers1 = markers1.to8UC1();
         //Invert
-        Imgproc.threshold(markers1, markers1, treshold, 255.0, Imgproc.THRESH_BINARY_INV)
+        Imgproc.threshold(uc8markers1, uc8markers1, treshold, 255.0, Imgproc.THRESH_BINARY_INV)
+
+
+        val processedmarkers = uc8markers1.to32SC1()
+        for (i in 0..processedmarkers.height() - 1) {
+            for (j in 0..processedmarkers.width() - 1) {
+                if (processedmarkers.get(i, j).get(0).equals(0.0)) {
+                    processedmarkers.put(i, j, 1.0)
+                }
+            }
+        }
 
         //watershed
-        Imgproc.watershed(image.image, markers1)
+        Imgproc.watershed(image.image, processedmarkers)
+        for (i in 0..processedmarkers.height() - 1) {
+            var print = ""
+            for (j in 0..processedmarkers.width() - 1) {
+                print += processedmarkers.get(i, j).get(0).toString() + " "
+            }
+            System.out.println(print)
+        }
         //markers1.convertTo(markers1, CvType.CV_8U)
 /*
         val mark = Mat.zeros(markers1.size(), CvType.CV_8UC1);
@@ -154,12 +248,22 @@ class NikieRoomDetection : IRoomDetectionAlgorithm {
         history.add(AFImage(bindist, "Markers"))
         history.add(AFImage(dist_8u, "Distu8"))
         history.add(AFImage(markers1, "Markers1"))
+        history.add(AFImage(uc8markers1, "uc8Markers1"))
+        history.add(AFImage(processedmarkers, "processedMarkers1"))
+
         history.add(AFImage(distMinus, "Dist Minus"))
         history.add(AFImage(geodesicDistTransform, "Geodesic"))
         history.add(AFImage(markersBefore, "Markers before"))
         history.add(AFImage(invDistTransform, "Inverse DistT"))
         history.add(AFImage(markers, "Markers"))
-
-        return AFImage(markers1)
+*/
+        history.add(AFImage(distTransform, "Distanztransformation"))
+        history.add(AFImage(geodesicTransform, "Geodesictransformation"))
+        history.add(AFImage(markers, "Markers"))
+        history.add(AFImage(foreground, "Foreground"))
+        history.add(AFImage(background, "Background"))
+        history.add(AFImage(summedUp, "Summed Up"))
+        history.add(AFImage(watershed, "Watershed"))
+        return AFImage(markers)
     }
 }
