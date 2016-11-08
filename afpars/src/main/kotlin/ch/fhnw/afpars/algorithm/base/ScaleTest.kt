5879659fcfd2a742c71558e674ba647e8bf37e06
Florian Bruggisser <florian@nexpose.ch>
2016-11-08 15:48:20 +0100

    implemented scale example

--- afpars/src/main/kotlin/ch/fhnw/afpars/algorithm/base/ScaleTest.kt
+++ afpars/src/main/kotlin/ch/fhnw/afpars/algorithm/base/ScaleTest.kt
@@ -2,6 +2,7 @@ package ch.fhnw.afpars.algorithm.base
 
 import ch.fhnw.afpars.algorithm.AlgorithmParameter
 import ch.fhnw.afpars.model.AFImage
+import ch.fhnw.afpars.util.copy
 import ch.fhnw.afpars.util.resize
 import org.opencv.core.Size
 import org.opencv.imgproc.Imgproc
@@ -15,19 +16,26 @@ class ScaleTest : IBaseAlgorithm {
 
     override fun run(image: AFImage, history: MutableList<AFImage>): AFImage {
 
-        val img = image.image
+        val img = image.image.copy()
         val images = mutableListOf(img)
         images.add(img.resize(img.width() / 2, 0))
         images.add(img.resize(img.width() / 4, 0))
         images.add(img.resize(img.width() / 8, 0))
         images.add(img.resize(img.width() / 16, 0))
 
-        val element = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, Size(2.0 * erosionSize + 1.0, 2.0 * erosionSize + 1.0))
+        val originalSize = img.width()
 
         // calculate threshold
         for (i in images) {
+            val relativeSize = i.width() / originalSize.toDouble()
+            val relativeErosion = relativeSize * erosionSize
+            val element = Imgproc.getStructuringElement(Imgproc.MORPH_RECT,
+                    Size(2.0 * relativeErosion + 1.0, 2.0 * relativeErosion + 1.0))
+
             Imgproc.erode(i, i, element)
             history.add(AFImage(i, "${i.size()}"))
+
+            println("${i.size()}:\tsize: $relativeSize\terosion: $relativeErosion")
         }
 
         return image
