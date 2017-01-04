dc094bc21847e0e15ea96aa10906263d5299b53c
Florian Bruggisser <florian@nexpose.ch>
2017-01-04 16:30:14 +0100

    implemented classifier with more negative samples

--- afpars/src/main/kotlin/ch/fhnw/afpars/algorithm/objectdetection/CascadeClassifierDetector.kt
+++ afpars/src/main/kotlin/ch/fhnw/afpars/algorithm/objectdetection/CascadeClassifierDetector.kt
@@ -33,7 +33,7 @@ class CascadeClassifierDetector : IObjectDetectionAlgorithm {
     val minNeighbors: Int = 3
 
     override fun run(image: AFImage, history: MutableList<AFImage>): AFImage {
-        val cascadeDetector = CascadeClassifier("cascade-files/cascade_thicken.xml")
+        val cascadeDetector = CascadeClassifier("cascade-files/cascade_neg.xml")
 
         // morphological transform
         val element = Imgproc.getStructuringElement(Imgproc.MORPH_RECT,
