a9e2c8b934d0c2bf00fc86f5c64c2267ee02cb29
Florian Bruggisser <florian@nexpose.ch>
2017-01-30 12:27:11 +0100

    implemented load from clipboard

--- afpars/src/main/kotlin/ch/fhnw/afpars/Main.kt
+++ afpars/src/main/kotlin/ch/fhnw/afpars/Main.kt
@@ -1,18 +1,25 @@
 package ch.fhnw.afpars
 
+import ch.fhnw.afpars.ui.controller.MainView
 import javafx.application.Application
 import javafx.fxml.FXMLLoader
 import javafx.scene.Parent
 import javafx.scene.Scene
 import javafx.stage.Stage
 
+
 class Main : Application() {
 
     @Throws(Exception::class)
     override fun start(primaryStage: Stage) {
-        val root = FXMLLoader.load<Parent>(javaClass.classLoader.getResource("view/MainView.fxml"))
+        val loader = FXMLLoader(javaClass.classLoader.getResource("view/MainView.fxml"))
+        val root = loader.load<Any>() as Parent
+        val controller = loader.getController<Any>() as MainView
+
+
         primaryStage.title = "Architectural Floor Plan Analysis"
         primaryStage.scene = Scene(root)
+        primaryStage.setOnShown { controller.setupView() }
         primaryStage.show()
         System.out.println()
     }
--- afpars/src/main/kotlin/ch/fhnw/afpars/ui/controller/MainView.kt
+++ afpars/src/main/kotlin/ch/fhnw/afpars/ui/controller/MainView.kt
@@ -3,14 +3,23 @@ package ch.fhnw.afpars.ui.controller
 import ch.fhnw.afpars.io.reader.AFImageReader
 import ch.fhnw.afpars.model.AFImage
 import ch.fhnw.afpars.ui.control.editor.ImageEditor
+import ch.fhnw.afpars.ui.control.editor.Layer
+import ch.fhnw.afpars.ui.control.editor.shapes.RectangleShape
+import ch.fhnw.afpars.util.toImage
+import ch.fhnw.afpars.util.toMat
 import javafx.beans.property.SimpleObjectProperty
 import javafx.event.ActionEvent
 import javafx.fxml.FXML
+import javafx.geometry.Dimension2D
 import javafx.scene.Node
+import javafx.scene.image.Image
+import javafx.scene.input.Clipboard
 import javafx.scene.layout.BorderPane
+import javafx.scene.paint.ImagePattern
 import javafx.stage.FileChooser
 import javafx.stage.Stage
 import org.opencv.core.Core
+
 
 /**
  * Created by cansik on 15.01.17.
@@ -20,19 +29,51 @@ class MainView {
 
     val statusText = SimpleObjectProperty<String>("Status")
 
+    val canvas = ImageEditor()
+
     @FXML
     var layoutPane: BorderPane? = null
 
     init {
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME)
+
+        image.addListener { o -> displayImage(image.value.image.toImage()) }
     }
 
-    fun initCanvas(e: ActionEvent) {
-        val canvas = ImageEditor()
+    fun setupView() {
+        // init canvas
         canvas.prefWidth(100.0)
         canvas.prefWidth(100.0)
 
         layoutPane!!.center = canvas
+    }
+
+    fun displayImage(image: Image) {
+        canvas.resizeCanvas(image.width, image.height)
+
+        // set layer
+        val imageLayer = Layer("Image")
+        val drawLayer = Layer("Draw")
+
+        val imageRect = RectangleShape()
+        imageRect.size = Dimension2D(image.width, image.height)
+        imageRect.noStroke()
+        imageRect.fill = ImagePattern(image, 0.0, 0.0, image.width, image.height, false)
+
+        imageLayer.shapes.add(imageRect)
+
+        canvas.layers.clear()
+        canvas.layers.add(imageLayer)
+        canvas.layers.add(drawLayer)
+        canvas.activeLayer = drawLayer
+        canvas.redraw()
+    }
+
+    fun loadImageFromClipBoard(e: ActionEvent) {
+        val cb = Clipboard.getSystemClipboard()
+        if (cb.hasImage()) {
+            image.set(AFImage(cb.image.toMat()))
+        }
     }
 
     fun loadImageFromFile(e: ActionEvent) {
