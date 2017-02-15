e2852af667fa71880336a6d9c4a4da639caf52d4
Alexander Wyss <a.swyss@bluewin.ch>
2017-02-15 13:13:09 +0100

    Readded

---
+++ afpars/src/main/kotlin/ch/fhnw/afpars/model/RoomPolygonShape.kt
@@ -0,0 +1,22 @@
+package ch.fhnw.afpars.model
+
+import ch.fhnw.afpars.ui.control.editor.shapes.PolygonShape
+import ch.fhnw.afpars.util.format
+import ch.fhnw.afpars.util.opencv.contour.Contour
+import javafx.geometry.Point2D
+
+/**
+ * Created by cansik on 15.02.17.
+ */
+class RoomPolygonShape(val contour: Contour, points: MutableList<Point2D> = mutableListOf<Point2D>()) : PolygonShape(points) {
+    var relation : Double = 0.0
+
+    fun areaInCentimeter() : Double
+    {
+        return area() * Math.pow(relation, 2.0)
+    }
+
+    override fun toString(): String {
+        return "Room (${(areaInCentimeter() / 10000.0).format(2)} m²)"
+    }
+}
